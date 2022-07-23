b7cec0212f52eca588e6d5876a0d43627b41e13f
chapel1337 <103341366+chapel1337@users.noreply.github.com>
2022-07-23 10:18:20 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+// written by chapel1337
+// 7/22/2022
+// took 2 hours to create
+
+function checkTime() {
+    let date = new Date();
+
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    let clockTime = document.getElementById("clock_time");
+    let clockSeconds = document.getElementById("clock_seconds");
+
+    // credit to jac
+    clockTime.innerHTML = (`${hours < 10 ? "0" : ""}${hours}:${minutes < 10 ? "0" : ""}${minutes}`);
+    clockSeconds.innerHTML = (`${seconds < 10 ? "0" : ""}${seconds}`);
+    // -------------
+}
+
+setInterval(checkTime, 1000);
